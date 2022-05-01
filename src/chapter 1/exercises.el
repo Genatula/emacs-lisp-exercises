6d21df329fc0dfaf25d6ecb15d208804d57330f4
Ilnas <ilnastheclown@gmail.com>
2022-05-01 12:57:17 +0300

    Commit first chapter exercises

---
+++ src/chapter 1/exercises.el
@@ -0,0 +1,18 @@
+;; Generate an error message by evaluating an appropriate symbol that is not within parantheses
+generate-error-message
+
+;; Generate an error message by evaluating an appropriate symbol that is between parantheses
+(generate-error-message)
+
+;; Create a counter that increments by two
+(setq counter 0)
+
+(setq counter (+ counter 2))
+
+counter
+
+;; Write an expression that prints a message to the echo area when evaluated
+(message "This message is printed to the echo area")
+
+
+
